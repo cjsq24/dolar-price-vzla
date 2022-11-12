b1d2fd57dc76267a191ef1e16a3413197cb69568
Carlos Sosa <cjsq24@gmail.com>
2022-11-12 17:09:14 -0400

    fix add images folder to build folder

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,35 @@
-import '@babel/polyfill';
-import http from 'http';
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import path from 'path';
+import "@babel/polyfill";
+import http from "http";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 //Para manejar las variables de entorno
-import 'dotenv/config';
+import "dotenv/config";
 
-import routes from './modules/routes';
-import dbConfig from './dbConfig';
-import './modules/cronJob/cronJob';
+import routes from "./modules/routes";
+import dbConfig from "./dbConfig";
+import "./modules/cronJob/cronJob";
 
 const app = express();
 const server = http.createServer(app);
 
 //middlewares
 //Para mostrar en la consola las peticiones
-app.use(morgan('dev'))
-app.use(cors())
+app.use(morgan("dev"));
+app.use(cors());
 //Para que el servidor pueda recibir en formato json
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use('/api', routes);
-app.use('/public',express.static(path.join(__dirname, './images')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api", routes);
+app.use("/public", express.static(path.join(__dirname, "./images")));
 
-app.get('/', (req, res) => {
-   res.send('Backend de dolar-price');
+app.get("/", (req, res) => {
+  res.send("Backend de dolar-price");
 });
 
 server.listen(process.env.PORT || 4000, () => {
-   console.log(`Server on port ${process.env.PORT || 4000}`);
-   dbConfig();
+  console.log(`Server on port ${process.env.PORT || 4000}`);
+  console.log("hi", path.join(__dirname, "./images"));
+  dbConfig();
 });
